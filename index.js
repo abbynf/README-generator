7ffdd85176cf33772e7f4b8fb0c154c7cb939799
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-09-23 20:41:59 -0600

    added functions to ask questions, create template, and write file to test efficacy

--- index.js
+++ index.js
@@ -4,16 +4,36 @@ var inquirer = require("inquirer");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What is your user name?",
+        name: "username"
+      }
 
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(data) {
+    fs.writeFile("example.md", data, function(err) {
+
+          if (err) {
+            return console.log(err);
+          }
+        
+          console.log("Success!");
+        
+        });
 }
 
 // function to initialize program
 function init() {
-
+    inquirer
+        .prompt(questions)
+        .then(function(response){
+            const markdownTemplate = 
+            `My name is ${response.username}`
+            writeToFile(markdownTemplate)
+        })
 }
 
 // function call to initialize program
