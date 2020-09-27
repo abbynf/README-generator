4d35e9b4bbba2bad9a08c57c5e09dc0c816d7c0b
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-09-26 20:28:29 -0600

    added questions for inquirer

--- index.js
+++ index.js
@@ -6,32 +6,72 @@ var inquirer = require("inquirer");
 const questions = [
     {
         type: "input",
-        message: "What is your user name?",
+        message: "What is the title of the project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is the description of the application?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Provide a step-by-step description of how to get the development environment running",
+        name: "installation" 
+    },
+    {
+        type: "input",
+        message: "Provide instructions and examples for use.",
+        name: "usage"
+    },
+    {
+        type: "list",
+        message: "Please choose a license.",
+        choices: ["MIT license", "GNU AGPLv3", "GNU GPLv3", "Mozilla Public License 2.0"],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so.",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "What tests do you have for your application? Provide examples on how to run the tests.",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "What is your github username?",
         name: "username"
-      }
-
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    }
 ];
 
 // function to write README file
 function writeToFile(data) {
-    fs.writeFile("example.md", data, function(err) {
+    fs.writeFile("example.md", data, function (err) {
 
-          if (err) {
+        if (err) {
             return console.log(err);
-          }
-        
-          console.log("Success!");
-        
-        });
+        }
+
+        console.log("Success!");
+
+    });
 }
 
 // function to initialize program
 function init() {
     inquirer
         .prompt(questions)
-        .then(function(response){
-            const markdownTemplate = 
-            `My name is ${response.username}`
+        .then(function (response) {
+            const markdownTemplate =
+                `My name is ${response.username}`
             writeToFile(markdownTemplate)
         })
 }
